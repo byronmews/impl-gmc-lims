559050f46f008e90a135d4276e963d8a9326023b
Graham Rose <graham@Grahams-MacBook.local>
2018-07-10 22:25:57 +0100

    frontend additions

--- forms/Form_New Participant.vb
+++ forms/Form_New Participant.vb
@@ -1,13 +1,15 @@
 ﻿Option Compare Database
 
+
+
 Private Sub ComboDiseaseType_Click()
     If ComboDiseaseType.Value = "Cancer" Then
         TabMain.Pages.Item(0).Visible = True
         TabMain.Pages.Item(1).Visible = False
         TabMain.Pages.Item(2).Visible = False
         
-        SQL = "SELECT DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, DEMOGRAPHICS.dob, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.gender FROM CANCER INNER JOIN DEMOGRAPHICS ON CANCER.nhs_number = DEMOGRAPHICS.nhs_number;"
-        
+        SQL = "SELECT DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, DEMOGRAPHICS.dob, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.gender, CANCER.* FROM CANCER INNER JOIN DEMOGRAPHICS ON CANCER.nhs_number_cancer = DEMOGRAPHICS.nhs_number;"
+
         Form.RecordSource = SQL
         
     
@@ -16,7 +18,7 @@ Private Sub ComboDiseaseType_Click()
         TabMain.Pages.Item(1).Visible = True
         TabMain.Pages.Item(2).Visible = False
         
-        SQL = "SELECT DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, DEMOGRAPHICS.dob, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.gender FROM HAEM INNER JOIN DEMOGRAPHICS ON HAEM.nhs_number = DEMOGRAPHICS.nhs_number;"
+        SQL = "SELECT DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, DEMOGRAPHICS.dob, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.gender, HAEM.* FROM HAEM INNER JOIN DEMOGRAPHICS ON HAEM.nhs_number_haem = DEMOGRAPHICS.nhs_number;"
         
         Form.RecordSource = SQL
     
@@ -26,7 +28,7 @@ Private Sub ComboDiseaseType_Click()
         TabMain.Pages.Item(1).Visible = False
         TabMain.Pages.Item(2).Visible = True
         
-        SQL = "SELECT DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, DEMOGRAPHICS.dob, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.gender FROM RD INNER JOIN DEMOGRAPHICS ON RD.nhs_number = DEMOGRAPHICS.nhs_number;"
+        SQL = "SELECT DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, DEMOGRAPHICS.dob, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.gender, RD.* FROM RD INNER JOIN DEMOGRAPHICS ON RD.nhs_number_rd = DEMOGRAPHICS.nhs_number;"
         
         Form.RecordSource = SQL
         
