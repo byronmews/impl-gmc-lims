70dce1e484fd54c3d9bd475ec00e60e3b9fd0c0d
Graham Rose <graham@Grahams-MacBook.local>
2018-07-11 10:30:03 +0100

    v0.8

--- forms/Form_New Participant.vb
+++ forms/Form_New Participant.vb
@@ -1,7 +1,8 @@
 ﻿Option Compare Database
 
 
-' Select database for record source of form. Defaults to Cancer table with all other TabMain pages not visible.
+
+' Select database for record source of main form and subform. Defaults to cancer table, hide all other disease TabMain pages.
 Private Sub ComboDiseaseType_Click()
 
     If ComboDiseaseType.Value = "Cancer" Then
@@ -9,13 +10,19 @@ Private Sub ComboDiseaseType_Click()
         TabMain.Pages.Item(1).Visible = False
         TabMain.Pages.Item(2).Visible = False
         
+        ' Join demographics and cancer tables
         SQL = "SELECT * FROM CANCER INNER JOIN DEMOGRAPHICS ON CANCER.nhs_number = DEMOGRAPHICS.nhs_number;"
 
+        ' Change main form and subform to disease specific records
         Form.RecordSource = SQL
         subCancerQueryAll.Form.RecordSource = SQL
         
+        ' Change main form to new record input
         DoCmd.GoToRecord , , acNewRec
         
+        ' Change disease_type combobox to cancer specific values
+        disease_type.RowSource = "Breast, Ovarian, Prostate, Lung, Colorectal, Sarcoma, Renal," & _
+                                    "Brain, Bladder, Endometrial, Testicular, GI, Pancreas"
         
     ElseIf ComboDiseaseType.Value = "Haem Oncology" Then
         
@@ -25,11 +32,14 @@ Private Sub ComboDiseaseType_Click()
         
         SQL = "SELECT * FROM HAEM INNER JOIN DEMOGRAPHICS ON HAEM.nhs_number = DEMOGRAPHICS.nhs_number;"
 
+        ' Change main form and subform to disease specific records
         Form.RecordSource = SQL
         subHaemQueryAll.Form.RecordSource = SQL
         
         DoCmd.GoToRecord , , acNewRec
         
+        ' Change disease_type combobox to cancer specific values
+        disease_type.RowSource = "AML, ALL, CML, CLL, Other"
                 
     ElseIf ComboDiseaseType.Value = "Rare Disease" Then
         
@@ -39,40 +49,80 @@ Private Sub ComboDiseaseType_Click()
         
         SQL = "SELECT * FROM RD INNER JOIN DEMOGRAPHICS ON RD.nhs_number = DEMOGRAPHICS.nhs_number;"
         
+        ' Change main form and subform to disease specific records
         Form.RecordSource = SQL
         subRDQueryAll.Form.RecordSource = SQL
         
         DoCmd.GoToRecord , , acNewRec
         
+        ' Change disease_type combobox to RD specific values
+        disease_type.RowSource = "Cardiovascular disorders, Dermatological disorders," & _
+                                    "Dysmorphic & congenital abnormality syn, Endocrine," & _
+                                    "Growth disorders, Haematological disorders, Hearing & ear disorders," & _
+                                    "Metabolic, Neurology & neurodevelopmental," & _
+                                    "Ophthalmological, Renal and urinary tract, Respiratory disorders," & _
+                                    "Skeletal, Rheumatological and connective tissue, Tumour predisposition syn, Ultrarare," & _
+                                    "Unknown at sample receipt"
+                                    
     End If
-        
+    
 End Sub
 
-' New record. Form defaults to Cancer table, select if new cancer record needed.
+
+' New record. Main form defaults to cancer table join
 Private Sub NewRecord_Click()
 
     ' Me!subCancerQueryAll.SetFocus
     DoCmd.GoToRecord , , acNewRec
     
+    ' When new participant is being entered, hide subform filter box until record is saved
     Me.BoxSearch.Visible = False
     Me.LabelTextSearch.Visible = False
     Me.TextSearch.Visible = False
+    Me.ClearTextSearch.Visible = False
 
 End Sub
 
-' Save record input
+' Save record input button
 Private Sub Save_Click()
 
-    DoCmd.RunCommand acCmdSaveRecord
+    ' Enforce required inputs are entered, if false then oppup box msg. Else continue the save form process.
+    If IsNull(first_name) Then
+        MsgBox "Enter first_name"
+        Cancel = True
+    ElseIf IsNull(surname) Then
+        MsgBox "Enter surname"
+        Cancel = True
+    ElseIf IsNull(nhs_number) Then
+        MsgBox "Enter NHS number"
+        Cancel = True
+    ElseIf IsNull(dob) Then
+        MsgBox "Enter dob"
+        Cancel = True
+    ElseIf IsNull(gender) Then
+        MsgBox "Enter gender"
+        Cancel = True
+    ElseIf IsNull(status_consent_date) Then
+        MsgBox "Enter status_consent_date"
+        Cancel = True
+    ElseIf IsNull(disease_type) Then
+        MsgBox "Enter Disease Type"
+        Cancel = True
+    Else
+       ' Save record
+        DoCmd.RunCommand acCmdSaveRecord
 
-    ' Refresh all tables
-    Me.Refresh
-    subCancerQueryAll.Form.Refresh
+        ' Refresh all tables
+        Me.Refresh
+        subCancerQueryAll.Form.Refresh
 
-    ' Enable search box again after new record input saved
-    Me.BoxSearch.Visible = True
-    Me.LabelTextSearch.Visible = True
-    Me.TextSearch.Visible = True
+        ' Enable filter textbox after new record input saved
+        Me.BoxSearch.Visible = True
+        Me.LabelTextSearch.Visible = True
+        Me.TextSearch.Visible = True
+        Me.ClearTextSearch.Visible = True
+    
+    End If
 
 End Sub
 
@@ -81,11 +131,10 @@ End Sub
 Private Sub TextSearch_Change()
     
     Dim strFilter As String
-    
     Me.Refresh
+    Me.TextBoxRecordCount.Visible = False
     
-    ' Number of conditions
-    'strFilter = "[surname] LIKE '*" & Me.TextSearch & "*'"
+    ' 2 conditions - gene_id_ & surname
     strFilter = "[genie_id] LIKE '*" & Me.TextSearch & "*' OR [surname] LIKE '*" & Me.TextSearch & "*' "
     
     If ComboDiseaseType.Value = "Cancer" Then
@@ -123,10 +172,11 @@ Private Sub TextSearch_Change()
 End Sub
 
 
-' Clear TextSearch value and remove filter
+' Clear TextSearch value, remove filter, make sample count box visible
 Private Sub ClearTextSearch_Click()
 
     Me.TextSearch.Value = ""
     Form.FilterOn = False
+    Me.TextBoxRecordCount.Visible = True
 
 End Sub
