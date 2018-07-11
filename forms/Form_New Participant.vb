46b6108b0f738da979abe82658439f9a66f55435
Graham Rose <graham@Grahams-MacBook.local>
2018-07-11 07:36:14 +0100

    all queries for participants form added

--- forms/Form_New Participant.vb
+++ forms/Form_New Participant.vb
@@ -1,38 +1,57 @@
 ﻿Option Compare Database
 
 
-
 Private Sub ComboDiseaseType_Click()
+
     If ComboDiseaseType.Value = "Cancer" Then
         TabMain.Pages.Item(0).Visible = True
         TabMain.Pages.Item(1).Visible = False
         TabMain.Pages.Item(2).Visible = False
         
-        SQL = "SELECT DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, DEMOGRAPHICS.dob, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.gender, CANCER.* FROM CANCER INNER JOIN DEMOGRAPHICS ON CANCER.nhs_number_cancer = DEMOGRAPHICS.nhs_number;"
+        SQL = "SELECT * FROM CANCER INNER JOIN DEMOGRAPHICS ON CANCER.nhs_number = DEMOGRAPHICS.nhs_number;"
 
         Form.RecordSource = SQL
+        subCancerQueryAll.Form.RecordSource = SQL
         
-    
+ 
+        
     ElseIf ComboDiseaseType.Value = "Haem Oncology" Then
+        
         TabMain.Pages.Item(0).Visible = False
         TabMain.Pages.Item(1).Visible = True
         TabMain.Pages.Item(2).Visible = False
         
-        SQL = "SELECT DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, DEMOGRAPHICS.dob, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.gender, HAEM.* FROM HAEM INNER JOIN DEMOGRAPHICS ON HAEM.nhs_number_haem = DEMOGRAPHICS.nhs_number;"
+        SQL = "SELECT * FROM HAEM INNER JOIN DEMOGRAPHICS ON HAEM.nhs_number = DEMOGRAPHICS.nhs_number;"
+
+        subHaemQueryAll.Form.RecordSource = SQL
         
-        Form.RecordSource = SQL
-    
-    
+                
     ElseIf ComboDiseaseType.Value = "Rare Disease" Then
+        
         TabMain.Pages.Item(0).Visible = False
         TabMain.Pages.Item(1).Visible = False
         TabMain.Pages.Item(2).Visible = True
         
-        SQL = "SELECT DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, DEMOGRAPHICS.dob, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.gender, RD.* FROM RD INNER JOIN DEMOGRAPHICS ON RD.nhs_number_rd = DEMOGRAPHICS.nhs_number;"
-        
-        Form.RecordSource = SQL
-        
+        SQL = "SELECT * FROM RD INNER JOIN DEMOGRAPHICS ON RD.nhs_number = DEMOGRAPHICS.nhs_number;"
+
+        subRDQueryAll.Form.RecordSource = SQL
         
     End If
-    
+        
+End Sub
+
+Private Sub NewRecord_Click()
+
+    ' Me!subCancerQueryAll.SetFocus
+    DoCmd.GoToRecord , , acNewRec
+
+
+End Sub
+
+Private Sub Save_Click()
+
+' subCancerQueryAll.SetFocus
+
+DoCmd.RunCommand acCmdSaveRecord
+
 End Sub
