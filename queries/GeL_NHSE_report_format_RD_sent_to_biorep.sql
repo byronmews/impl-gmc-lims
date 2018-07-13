fa9cfa9941a79110b201f386408a3bad54890228
Graham Rose <grahamdrose@gmail.com>
2018-07-13 08:05:51 +0100

    new queries

---
+++ queries/GeL_NHSE_report_format_RD_sent_to_biorep.sql
@@ -0,0 +1,7 @@
+SELECT RD.genie_id, DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, RD.genie_family_ID, RD.status_consent_date, RD.received_date, RD.recruiter, RD.status_impl_to_gosh_dispatch_date, RD.comment, SWITCH
+        (
+        RD.genie_family_id LIKE 'RYJ*','ICHT',
+        RD.genie_family_id LIKE 'RQM*','C&W',
+        (RD.genie_family_id NOT LIKE 'RYJ*' OR RD.genie_family_id NOT LIKE 'RQM*'),'NOT ON GENIE') AS LDP
+FROM RD INNER JOIN DEMOGRAPHICS ON RD.[nhs_number] = DEMOGRAPHICS.[nhs_number]
+WHERE RD.status_impl_to_gosh_dispatch_date IS NOT NULL;
