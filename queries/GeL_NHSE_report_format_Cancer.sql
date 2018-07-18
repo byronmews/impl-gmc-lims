b56d4f7355798495263c134400665b0718d58dd9
Graham Rose <grahamdrose@gmail.com>
2018-07-18 16:35:19 +0100

    update v1.0

--- queries/GeL_NHSE_report_format_Cancer.sql
+++ queries/GeL_NHSE_report_format_Cancer.sql
@@ -1,7 +1,8 @@
 SELECT CANCER.genie_id, DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, CANCER.hospital, CANCER.status_blood, CANCER.[status_comments/actions_(blood)], CANCER.status_tissue, CANCER.[status_comments/actions_(tissue)], CANCER.disease_type, CANCER.[status_eligible_(y/n)], CANCER.status_impl_to_gosh_dispatch_date, SWITCH
          (
         CANCER.hospital = 'HH', 'ICHT',
-        CANCER.hospital = 'QCH', 'ICHT',
+        CANCER.hospital
+= 'QCH', 'ICHT',
         CANCER.hospital = 'CXH', 'ICHT',
         CANCER.hospital = 'SMH', 'ICHT',
         CANCER.hospital = 'ChelWest', 'C&W',
@@ -9,7 +10,7 @@ SELECT CANCER.genie_id, DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, CANCER.ho
         CANCER.hospital = 'WMH', 'C&W',
         CANCER.hospital = 'C&W', 'C&W'
         ) AS LDP, SWITCH
-        (
+(
         CANCER.status_blood ='Y' AND CANCER.status_tissue ='Y' 
         AND CANCER.status_impl_to_gosh_dispatch_date IS NULL
         AND CANCER.[status_comments/actions_(tissue)] NOT LIKE 'extracted'
@@ -17,12 +18,12 @@ SELECT CANCER.genie_id, DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, CANCER.ho
         
         CANCER.status_blood = 'Y' AND CANCER.status_tissue = 'Y' 
         AND CANCER.[status_comments/actions_(tissue)] NOT LIKE 'to QC'
-        AND 
-                (
+        AND
+(
                 CANCER.status_impl_to_gosh_dispatch_date IS NOT NULL OR CANCER.[status_comments/actions_(tissue)] LIKE 'extracted*'
                 ), 'Samples Passed tissue QC in GMC',
 
-        CANCER.[status_comments/actions_(tissue)] LIKE '*fail*' OR 
-        (CANCER.status_tissue <> 'Y' OR CANCER.status_tissue IS NULL), 'Samples not paired or failed QC steps',
+        CANCER.[status_comments/actions_(tissue)] LIKE '*fail*' OR
+(CANCER.status_tissue <> 'Y' OR CANCER.status_tissue IS NULL), 'Samples not paired or failed QC steps',
         ) AS GEL_QC_STAGE
 FROM CANCER INNER JOIN DEMOGRAPHICS ON CANCER.[nhs_number] = DEMOGRAPHICS.[nhs_number];
--- queries/GeL_NHSE_report_format_RD_sent_to_biorep.sql
+++ queries/GeL_NHSE_report_format_RD_sent_to_biorep.sql
@@ -1,7 +1,9 @@
-SELECT RD.genie_id, DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, RD.genie_family_ID, RD.status_consent_date, RD.received_date, RD.recruiter, RD.status_impl_to_gosh_dispatch_date, RD.comment, SWITCH
+SELECT RD.genie_id, DEMOGRAPHICS.nhs_number, DEMOGRAPHICS.first_name, DEMOGRAPHICS.surname, RD.genie_family_ID, RD.status_consent_date, RD.received_date, RD.recruiter, RD.status_impl_to_gosh_dispatch_date, RD.comment, SWITCH
         (
         RD.genie_family_id LIKE 'RYJ*','ICHT',
-        RD.genie_family_id LIKE 'RQM*','C&W',
-        (RD.genie_family_id NOT LIKE 'RYJ*' OR RD.genie_family_id NOT LIKE 'RQM*'),'NOT ON GENIE') AS LDP
+        RD.genie_family_id
+LIKE 'RQM*','C&W',
+(RD.genie_family_id NOT LIKE 'RYJ*' OR RD.genie_family_id NOT LIKE 'RQM*' OR RD.genie_family_id IS NULL),'NOT ON GENIE'
+        ) AS LDP
 FROM RD INNER JOIN DEMOGRAPHICS ON RD.[nhs_number] = DEMOGRAPHICS.[nhs_number]
 WHERE RD.status_impl_to_gosh_dispatch_date IS NOT NULL;
